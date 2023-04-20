332d9dfdf903be955a1424e5948f141bc046bd48
Joe Binder <binderjoe@users.noreply.github.com>
2023-04-20 00:01:25 +0000

    Fix azd demo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,4 @@
-#!/bin/sh
-PLUGIN_HOSTNAME=$(echo "$PLUGIN_HOSTNAME" | sed 's/^"//' | sed 's/"$//')
-sed -i 's|https://your-app-url.com|'$PLUGIN_HOSTNAME'|g' ./.well-known/ai-plugin.json ./.well-known/openapi.yaml ./main.py
-
-exec uvicorn main:app --host 0.0.0.0 --port "${PORT:-${WEBSITES_PORT:-8080}}"
-
-
-#!/bin/sh
-set -eu
+set -e
 
 ./hostconfig.sh
 
